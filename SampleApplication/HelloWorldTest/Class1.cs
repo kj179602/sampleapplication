273b3aba80888f7020cf1ff8d60cdd65bef432b6
Katarzyna <kaj915@wp.pl>
2015-06-16 13:40:25 +0200

    Dodanie nowej Aplikacji testowej

---
+++ SampleApplication/HelloWorldTest/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SampleApplication.Controllers;
+using System.Web.Mvc;
+using System.Web;
+//using HelloWorldController;
+
+namespace HelloWorldTest
+{
+    [TestFixture]
+    public class HelloWorldControllerTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            string expected = "HelloWorld";
+            HelloWorldController controller = new HelloWorldController();
+            var result = controller.Index();
+            Assert.AreEqual(expected, result); 
+           
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            HelloWorldController controller = new HelloWorldController();
+            var result = controller.Welcome();
+            Assert.AreEqual("This is the Welcome action method...", result);
+
+        }
+
+
+    }
+}
---
+++ SampleApplication/SampleApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc; 
+ 
+namespace SampleApplication.Controllers 
+{ 
+    public class HelloWorldController : Controller 
+    { 
+        // 
+        // GET: /HelloWorld/ 
+ 
+        public string Index() 
+        { 
+            return "HelloWorld"; 
+        } 
+ 
+        // 
+        // GET: /HelloWorld/Welcome/ 
+ 
+        public string Welcome() 
+        { 
+            return "This is the Welcome action method..."; 
+        } 
+    } 
+}
